529fcdd032cf5d74619d8eaad089a24bda68a2ff
Milica Sucevic <milicasucevic@gmail.com>
2022-12-06 11:33:13 +0000

    add section-2

---
+++ section-3/terraform-manifests/ec2-instance.tf
@@ -0,0 +1,15 @@
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t3.micro"
+  user_data     = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
+
+# Key Pair Block
+resource "aws_key_pair" "ec2key" {
+  key_name   = "publicKey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
