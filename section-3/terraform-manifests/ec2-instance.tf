135a0c8f8bf82b37cabd19b1970ab13e3c921562
Milica Sucevic <milicasucevic@gmail.com>
2022-12-06 11:43:15 +0000

    add ec2 key pair

--- section-3/terraform-manifests/ec2-instance.tf
+++ section-3/terraform-manifests/ec2-instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "myec2vm" {
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t3.micro"
   user_data     = file("${path.module}/app1-install.sh")
+  key_name      = aws_key_pair.ec2key.id
   tags = {
     "Name" = "EC2 Demo"
   }
