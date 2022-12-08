3c106c6807418ad05dd3f63708087ffe95f7cf29
Milica Sucevic <milicasucevic@gmail.com>
2022-12-08 10:43:58 +0000

    initial commit vpc, ec2, sg modules

---
+++ section-7/ec2-instance-bastion.tf
@@ -0,0 +1,14 @@
+module "ec2-public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.2.1"
+
+  name = "${var.environment}-BastionHost"
+
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = local.common_tags
+}
