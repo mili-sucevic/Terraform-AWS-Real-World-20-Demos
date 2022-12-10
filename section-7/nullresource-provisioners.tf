b874ba6726bc1f1e7f17273cd7040e4c81b24acc
Milica Sucevic <milicasucevic@gmail.com>
2022-12-10 10:26:23 +0000

    final commit of the demo

--- section-7/nullresource-provisioners.tf
+++ section-7/nullresource-provisioners.tf
@@ -12,21 +12,24 @@ resource "null_resource" "name" {
 
   ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
-    source      = "private-key/terraform-key.pem"
+    source      = "private-key/tf-deploy.pem"
     destination = "/tmp/terraform-key.pem"
   }
+
   ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
       "sudo chmod 400 /tmp/terraform-key.pem"
     ]
   }
+
   ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
   provisioner "local-exec" {
     command     = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
     working_dir = "local-exec-output-files/"
     #on_failure = continue
   }
+
   ## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
   /*  provisioner "local-exec" {
     command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
--- section-7/versions.tf
+++ section-7/versions.tf
@@ -6,6 +6,11 @@ terraform {
       source = "hashicorp/aws"
       # version = "~> 3.0"
     }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0.0"
+    }
   }
 }
 
