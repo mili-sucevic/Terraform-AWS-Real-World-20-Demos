1ab9121172941a3734c30315daa4a38e7cfe8f0b
Milica Sucevic <milicasucevic@gmail.com>
2022-12-12 08:44:15 +0000

    fix formatting

--- section-7/elastic-ip.tf
+++ section-7/elastic-ip.tf
@@ -2,15 +2,15 @@
 # Resource - depends_on Meta-Argument
 resource "aws_eip" "bastion_eip" {
   depends_on = [ module.ec2_public, module.vpc ]
-  instance = module.ec2_public.id
-  vpc      = true
-  tags = local.common_tags
+  instance   = module.ec2_public.id
+  vpc        = true
+  tags       = local.common_tags
 
 ## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
   provisioner "local-exec" {
-    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    command     = "echo Destroy time prov `date` >> destroy-time-prov.txt"
     working_dir = "local-exec-output-files/"
-    when = destroy
+    when        = destroy
     #on_failure = continue
   }  
 }
--- section-7/local-vars.tf
+++ section-7/local-vars.tf
@@ -1,10 +1,10 @@
 # Define Local Values in Terraform
 locals {
-  owners      = var.business_division
-  environment = var.environment
-  name        = "${var.business_division}-${var.environment}"
+  owners        = var.business_division
+  environment   = var.environment
+  name          = "${var.business_division}-${var.environment}"
   #name = "${local.owners}-${local.environment}"
-  common_tags = {
+  common_tags   = {
     owners      = local.owners
     environment = local.environment
   }
