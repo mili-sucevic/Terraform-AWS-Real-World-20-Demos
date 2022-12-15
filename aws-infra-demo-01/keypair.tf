548532a81e1924bfac30e96884fba1dc616505f8
Milica Sucevic <milicasucevic@gmail.com>
2022-12-15 14:14:06 +0000

    clean-up old repos

--- aws-infra-demo-01/keypair.tf
+++
@@ -1,9 +0,0 @@
-# Creating a New Key
-resource "aws_key_pair" "keypair" {
-
-  # Name of the Key
-  key_name = var.keypair
-
-  # Adding the SSH authorized key !
-  public_key = file("~/.ssh/id_rsa.pub")
-}
